34e054a04f824151b331a9921e4ee045307e48b2
Li Xiao <iam@li-xiao.com>
2009-01-10 17:33:34 +0800

    fixed gadget for version 0.2

--- examples/gadget/mpage.js
+++ examples/gadget/mpage.js
@@ -1,10 +1,9 @@
 var prefs = new _IG_Prefs();
-var mpageURL = prefs.getString("mpageURL").strip();
-
-function responseHandler(response) {
+var mpageURL = prefs.getString("mpageURL");
+function mpageCallback(response) {
   var html = '<link href="'+ response.stylesheet +'" rel="Stylesheet" type="text/css"/>';
   html += "<div id=\"content\" class=\"wiki\">" + response.content + "</div>";
-  $('panel').innerHTML = html
+  document.getElementById('panel').innerHTML = html
   _IG_AdjustIFrameHeight();
 }
 
